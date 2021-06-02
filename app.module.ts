91d00bd9f71f02b0fb5ed8536b300995f08aab77
Thanhnhi47 <73121135+Thanhnhi47@users.noreply.github.com>
2021-06-02 17:10:56 +0700

    app

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+// import { AutofocusModule } from 'angular-autofocus-fix';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    // AutofocusModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todolist.module.ts
@@ -0,0 +1,13 @@
+export class todolist {
+  id: number;
+  title: String | undefined;
+  completed: boolean;
+  editing: boolean;
+
+  constructor() {
+    this.id = 0;
+    this.title = '';
+    this.completed = false;
+    this.editing = false;
+  }
+}
